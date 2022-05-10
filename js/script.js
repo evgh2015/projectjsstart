14237878233db8ff782472187228f78d206df0b3
Evgenia <evgenia-m@yandex.ru>
2022-05-10 16:20:04 +0200

    first

---
+++ js/script.js
@@ -0,0 +1,38 @@
+/* let number = 5;
+const leftBorderWidth = 1;
+number = 10;
+console.log(number);
+
+const obj = {
+    a: 50
+};
+
+obj.a = 10;
+
+console.log(obj);
+
+а = 15;
+console.log(a); */
+
+/* alert('Hello'); */
+
+/* const result = confirm("Are you here?");
+console.log(result); */
+
+/* const answer = prompt("Вам есть 18?", "");
+console.log(typeof(answer)); */
+
+/* const answer = [];
+answer[0] = prompt('Как ваше имя?', '');
+answer[1] = prompt('Ваша фамилия?', '');
+answer[2] = prompt('Сколько вам лет?', '');
+
+document.write(answer); */
+
+const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5`);
+
+const user = 'Ivan';
+
+alert(`Привет, ${user}`);
